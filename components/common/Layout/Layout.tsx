20f37290e2648b20da8c375b58a67e3c55be2f35
Kushal Raut <kushal.raut@dghub.io>
2022-06-21 21:50:30 +0545

    Structuring Styling Structure and Adding Colors

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,16 @@
 import React, { ReactNode } from 'react'
+import s from './Layout.module.css'
 
 interface Props {
   children?: ReactNode[] | ReactNode
 }
 
 const Layout: React.FC = ({ children }: Props) => {
-  return <div className="layout">{children}</div>
+  return (
+    <div className={s.root}>
+      <main className="fit">{children}</main>
+    </div>
+  )
 }
 
 export default Layout
